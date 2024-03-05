a9ac3d31c8135081e88c00c95fdcda773c89a32c
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-03-05 20:17:18 -0300

    adiciona arquivo app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+bairros_atendidos = ['bom retiro', 'butantã']
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/meu-bairro/<cep>")
+def new_route(cep):
+    print(f'O CEP digitado foi: {cep}')
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    resp = requests.get(url)
+    resp_dict = resp.json()
+    bairro = resp_dict.get('bairro')
+
+    if bairro is None:
+        return "Bairro não encontrado", 404
+
+    if bairro.lower() not in bairros_atendidos:
+        return f"Bairro {bairro} não atendido"
+
+    return f'Atendemos no bairro {bairro}'
+
+
+@app.route("/not-found")
+def not_found():
+    return "Página não encontrada", 404
